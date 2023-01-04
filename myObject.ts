22b119570fcfcc973c3c7129c1296fd8e34c05f4
Mukesh-a <mukeshabhi08@gmail.com>
2023-01-04 19:09:29 +0530

    Day 2

---
+++ myObject.ts
@@ -0,0 +1,8 @@
+function createUser({name:string,isPaid:boolean}){
+
+}
+createUser({name:"mukesh", isPaid:false})
+
+function createCourse():{name:string,price:boolean}{
+    return {name:"m",price:true}
+}
