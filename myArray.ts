1e9f45ed6a2421d1a3850d1be1e2144561157cbc
Mukesh-a <mukeshabhi08@gmail.com>
2023-01-04 22:47:15 +0530

    Day 2

--- myArray.ts
+++ myArray.ts
@@ -1,3 +1,12 @@
 const superHero: string [] = []
+const superHeros: number[] = []
 
+type User = {
+    name:string;
+    isActive:boolean
+}
+
+const allUser : User []=[]
+
+allUser.push({name:"", isActive : true})
 superHero.push("mukesh")
