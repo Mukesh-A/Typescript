48731e99b67a515f2368f208243b2e281b7fea82
Mukesh-a <mukeshabhi08@gmail.com>
2023-01-04 20:09:40 +0530

    Day 2

--- myObject.ts
+++ myObject.ts
@@ -6,3 +6,39 @@ createUser({name:"mukesh", isPaid:false})
 function createCourse():{name:string,price:boolean}{
     return {name:"m",price:true}
 }
+
+//Alias
+
+type User = {
+    name:string;
+    age:number;
+    isActive:boolean
+}
+
+function createUsers(user:User):User{
+    return {name:"",age:2,isActive:true}
+}
+
+
+//readonly, optional
+
+type Users = {
+    readonly _id:string;
+    age:number;
+    email:string
+}
+
+let user :Users = {
+    _id:"123",
+    age:73,
+    email:"string"
+}
+
+user._id = "" //gibing error
+
+
+type Userss = {
+    readonly _id:string;
+    age:number;
+    email?:string
+}
